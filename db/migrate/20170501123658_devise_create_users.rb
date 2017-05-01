10d9030735eccfb12591be6651c6cb8c55f451c8
Evgeny Belorusets <evgenybelorusets@gmail.com>
2017-05-01 18:13:18 +0300

    removed password

--- db/migrate/20170501123658_devise_create_users.rb
+++ db/migrate/20170501123658_devise_create_users.rb
@@ -1,10 +1,8 @@
 class DeviseCreateUsers < ActiveRecord::Migration[5.1]
   def change
     create_table :users do |t|
-      ## Database authenticatable
       t.string :battle_tag
       t.string :battle_id
-      t.string :encrypted_password, null: false, default: ""
     end
   end
 end
